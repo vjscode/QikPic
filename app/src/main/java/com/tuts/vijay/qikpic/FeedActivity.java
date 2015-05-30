30eef3debaf475f1634f6ff7c9f3464c3195b2b1
Vijay Sailappan <vijay.sailappan@gmail.com>
2015-05-30 12:34:41 -0700

    Push code to capture pictures

--- app/src/main/java/com/tuts/vijay/qikpic/FeedActivity.java
+++ app/src/main/java/com/tuts/vijay/qikpic/FeedActivity.java
@@ -1,17 +1,45 @@
 package com.tuts.vijay.qikpic;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
-public class FeedActivity extends Activity {
+public class FeedActivity extends Activity implements View.OnClickListener {
+
+    private static final int TAKE_PHOTO = 0;
+    ImageView captureBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+        captureBtn = (ImageView) findViewById(R.id.capture);
+        captureBtn.setOnClickListener(this);
     }
 
 
@@ -32,5 +60,81 @@ public class FeedActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.capture) {
+            startCamera();
+        }
+    }
+
+    private void startCamera() {
+        try {
+            createImageFileUri();
+            dispatchTakePictureIntent();
+        } catch (IOException e) {
+            Toast.makeText(this, "Could n't start camera", Toast.LENGTH_SHORT);
+        }
+    }
+
+    Uri mCurrentPhotoUri;
+
+    private void createImageFileUri() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        //mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        mCurrentPhotoUri = Uri.fromFile(image);
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, mCurrentPhotoUri);
+        startActivityForResult(takePictureIntent, TAKE_PHOTO);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bmp;
+        if (resultCode == RESULT_OK) {
+            try {
+                bmp = MediaStore.Images.Media.getBitmap( this.getContentResolver(), mCurrentPhotoUri);
+                prepareAndSaveParseObject(bmp);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void prepareAndSaveParseObject(Bitmap bmp) {
+        ParseObject po = new ParseObject("QikPik");
+        po.put("user", ParseUser.getCurrentUser());
+        po.put("image", getParseFileFromBitmap(bmp));
+        po.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                Log.d("test", "Success: " + e);
+            }
+        });
+    }
+
+    private ParseFile getParseFileFromBitmap(Bitmap bmp) {
+        ByteArrayOutputStream blob = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.PNG, 0 /* ignored for PNG */,blob);
+        byte[] imgArray = blob.toByteArray();
+        //Assign Byte array to ParseFile
+        ParseFile parseImagefile = new ParseFile("profile_pic.png", imgArray);
+        return parseImagefile;
     }
 }
