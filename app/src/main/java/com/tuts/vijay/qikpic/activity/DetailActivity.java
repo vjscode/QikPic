f89c6a66c261959051dfabf95921ddafa21d1cd7
Vijay Sailappan <vijay.sailappan@gmail.com>
2015-06-13 22:01:10 -0700

    Add tagging feature

--- app/src/main/java/com/tuts/vijay/qikpic/activity/DetailActivity.java
+++ app/src/main/java/com/tuts/vijay/qikpic/activity/DetailActivity.java
@@ -1,9 +1,15 @@
 package com.tuts.vijay.qikpic.activity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -17,16 +23,18 @@ import com.squareup.picasso.Picasso;
 import com.tuts.vijay.qikpic.R;
 import com.tuts.vijay.qikpic.Utils.DisplayUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Activity which displays a registration screen to the user.
  */
-public class DetailActivity extends Activity {
+public class DetailActivity extends Activity implements View.OnClickListener {
 
     private String id;
     ParseImageView imageView;
     LinearLayout tagPanel;
+    ImageView addTagIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +42,8 @@ public class DetailActivity extends Activity {
         setContentView(R.layout.activity_list_item_detail);
         imageView = (ParseImageView) findViewById(R.id.image);
         tagPanel = (LinearLayout) findViewById(R.id.taggingPanel);
+        addTagIcon = (ImageView) findViewById(R.id.addTagIcon);
+        addTagIcon.setOnClickListener(this);
         id = getIntent().getStringExtra("id");
         loadImage();
     }
@@ -60,7 +70,10 @@ public class DetailActivity extends Activity {
             return;
         }
         for (String tag: tagList) {
-            TextView tagView = new TextView(this);
+
+            LayoutInflater inflater = LayoutInflater.from(this);
+            TextView tagView= (TextView) inflater.inflate(R.layout.tag_view, null, false);
+            tagView.setText(tag);
             tagPanel.addView(tagView, 1, lp);
         }
     }
@@ -69,4 +82,50 @@ public class DetailActivity extends Activity {
         ParseFile f = object.getParseFile("image");
         Picasso.with(this).load(f.getUrl()).into(imageView);
     }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.addTagIcon) {
+            getInputFromUser();
+        }
+    }
+
+    private void getInputFromUser() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+        alert.setTitle("Tag");
+
+        final EditText input = new EditText(this);
+        alert.setView(input);
+
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                String value = input.getText().toString();
+                saveTag(value);
+            }
+        });
+
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // Canceled.
+            }
+        });
+
+        alert.show();
+   }
+
+   private void saveTag(final String tag) {
+       ParseQuery<ParseObject> query = ParseQuery.getQuery("QikPik");
+       query.getInBackground(id, new GetCallback<ParseObject>() {
+           public void done(ParseObject object, ParseException e) {
+               List<String> tags = object.getList("tags");
+               if (tags == null) {
+                   tags = new ArrayList<String>();
+               }
+               tags.add(tag);
+               object.put("tags", tags);
+               object.saveInBackground();
+           }
+       });
+   }
 }
